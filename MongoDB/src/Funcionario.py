2b64e2c5463520550f0b51202d31c12b086a38fb
claudius <60759342+claudio966@users.noreply.github.com>
2021-05-06 00:29:51 +0000

    Diretório MongoDB removido

--- MongoDB/src/Funcionario.py
+++
@@ -1,7 +0,0 @@
-import Pessoa
-
-class Funcionario(Pessoa.Pessoa):
-    def __init__(self,id,nome,email,senha, telefone,cargo):
-        super().__init__(id,nome,email,senha, telefone)
-        self.cargo = cargo
-    
--- MongoDB/src/Tecnico.py
+++
@@ -1,7 +0,0 @@
-import Funcionario
-
-class tecnico(Funcionario.Funcionario):
-    def __init__(self,id,nome,email,senha, telefone,cargo, estaDisponivel):
-        super().__init__(id,nome,email,senha,telefone,cargo)
-        self.estaDisponivel = estaDisponivel
-    
